a72f3763a6d8caba93f5d654ee9de4ee014a4633
Elias Vieira <el.vieira9@gmail.com>
2026-08-21 20:49:13 -0300

    Adição da opção de ajuda no header

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -29,6 +29,11 @@
         </router-link>
       </li>
       <li>
+        <router-link :to="{ name: 'help' }" @click="isMobileMenuOpen = false">
+          {{ $t("menu.help") }}
+        </router-link>
+      </li>
+      <li>
         <router-link :to="{ name: 'contact' }" @click="isMobileMenuOpen = false">
           {{ $t("menu.contact") }}
         </router-link>
